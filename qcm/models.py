7a2c26ac1e0cce427ebdf6c6f39e2d268ac058d6
Philippe Demaria <philippe.demaria83@gmail.com>
2022-08-22 23:27:13 +0100

    Update models.py

--- qcm/models.py
+++ qcm/models.py
@@ -923,7 +923,7 @@ class Parcours(ModelWithCode):
         data = {}
         today = timezone.now()
         if self.is_sequence :
-            nb_exercises  = self.parcours_relationship.filter(type_id=0, students = student,is_publish = 1).values_list("document_id",flat=True).distinct().count() 
+            nb_exercises  = self.parcours_relationship.filter(type_id=0, students = student,is_publish = 1).count() 
             quizzes       = self.parcours_relationship.filter(type_id=3, students = student,is_publish = 1)
             nb_q = 0
             for quizz in quizzes :
@@ -932,7 +932,7 @@ class Parcours(ModelWithCode):
 
 
         else :
-            nb_exercises  = self.parcours_relationship.filter( exercise__supportfile__is_title=0 , students = student,is_publish = 1).values_list("id",flat=True).distinct().count()  
+            nb_exercises  = self.parcours_relationship.filter( exercise__supportfile__is_title=0 , students = student,is_publish = 1).count()  
             quizzes       = self.quizz.filter( students = student)
             nb_q = 0
             for quizz in quizzes :
